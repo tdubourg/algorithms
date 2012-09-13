22ed805df23308b34a6d2f9ed6c13ef67f315657
troll <troll@troll-fixe-ubuntu.(none)>
2012-09-13 23:46:11 +0200

    A bit of math, Newtons iterative sqrt

---
+++ math/sqrt.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import math
+# Newton's iterative method
+
+def sqrt(a, precision):
+	if 1 == a:
+		return 1
+	if 0 == a:
+		return 0
+
+	if a < 0:
+		return float('nan')
+
+	r = 1
+	old_r = r
+	while True:
+		old_r = r
+		r = 0.5 * (r + a/r)
+		if (r*r) == a: # "exact" sqrt found
+			print "Stopping by exactness"
+			return r
+		if math.fabs(old_r-r)<=precision:
+			print "Stopping by precision reached"
+			return r
+
+
+print "Please tell us the precision to use for sqrt() results"
+precision = float(raw_input())
+
+while True:
+	print "Number?"
+	print sqrt(float(raw_input()), precision)
